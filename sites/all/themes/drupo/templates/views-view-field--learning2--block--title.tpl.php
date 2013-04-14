281eb0480070a2a51cf73186195a42966751de89
Mukhametdinov Fail <mufanu@gmail.com>
2013-04-14 11:06:26 +0400

    Changed text information

--- sites/all/themes/drupo/templates/views-view-field--learning2--block--title.tpl.php
+++ sites/all/themes/drupo/templates/views-view-field--learning2--block--title.tpl.php
@@ -24,7 +24,7 @@
 ?>
 <?php $status = $row->_field_data['nid']['entity']->status; ?>
 <?php if($status == 0): ?>
-  <span class="course-unpublished"><?php print $output; ?> (Снят с публикации)</span>
+  <span class="course-unpublished"><?php print $output; ?> (Снято с публикации)</span>
 <?php else: ?>
   <?php print $output; ?>
 <?php endif; ?>
